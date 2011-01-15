801f1e260b4e64e0504db01266c1ad73791c8805
Corbin Simpson <simpsoco@osuosl.org>
2011-01-14 16:44:38 -0800

    Slowly getting things into shape...

--- control.py
+++ control.py
@@ -1,13 +1,27 @@
+from twisted.internet import reactor
+from twisted.internet.error import CannotListenError
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import LineReceiver
+from twisted.python import log
+
+from factory import VNCProxy
 
 class ControlProtocol(LineReceiver):
 
     def lineReceived(self, line):
         try:
-            source, host, dest, password = line.split(":", 3)
+            sport, host, dport, password = line.split(":", 3)
+            sport = int(sport)
+            dport = int(dport)
+            factory = VNCProxy(host, dport, password)
+            reactor.listenTCP(sport, factory)
+            log.msg("New forwarder (%d->%s:%d)" % (sport, host, dport))
             self.sendLine("OK")
         except ValueError:
+            log.err("Couldn't handle line %r" % line)
+            self.sendLine("FAILED")
+        except CannotListenError:
+            log.err("Couldn't bind port %d" % sport)
             self.sendLine("FAILED")
 
         self.transport.loseConnection()
--- factory.py
+++ factory.py
@@ -7,12 +7,16 @@ class VNCProxy(Factory):
 
     protocol = VNCServerAuthenticator
 
-    def __init__(self):
-        self.cc = ClientCreator(reactor, VNCClientAuthenticator)
+    def __init__(self, host, port, password):
+        self.cc = ClientCreator(reactor, VNCClientAuthenticator, password)
+
+        self.host = host
+        self.port = port
+        self.password = password
 
     def makeProtocol(self, a):
-        server = Factory.makeProtocol(self, a)
-        d = self.cc.connectTCP()
+        server = VNCServerAuthenticator(self.password)
+        d = self.cc.connectTCP(self.host, self.port)
 
         d.addCallback(lambda client: (server, client))
         d.addCallback(self.client_connected)
