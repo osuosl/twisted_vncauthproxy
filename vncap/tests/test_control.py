fc7dd80c42c1188cf0c1fdfff0e2523f6a5abf30
Corbin Simpson <simpsoco@osuosl.org>
2011-01-20 14:02:05 -0800

    Create vncap package and start testing.

---
+++ vncap/tests/test_control.py
@@ -0,0 +1,44 @@
+from twisted.trial import unittest
+
+from vncap.control import ControlProtocol, ControlFactory
+
+class DummyTransport(object):
+
+    def __init__(self):
+        self.buf = ""
+
+    def write(self, data):
+        self.buf += data
+
+    def loseConnection(self):
+        pass
+
+class TestControlProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.cp = ControlProtocol()
+
+    def test_trivial(self):
+        pass
+
+class TestControlProtocolUsage(unittest.TestCase):
+
+    def setUp(self):
+        self.cp = ControlProtocol()
+        self.cp.transport = DummyTransport()
+
+    def test_trivial(self):
+        pass
+
+    def test_bad_data(self):
+        l = [
+            "herp:derp",
+            "herp:derp:lerp:merp",
+            ":::::",
+            "12:asdf:13",
+        ]
+        for line in l:
+            self.cp.lineReceived(line)
+            self.assertTrue(self.cp.transport.buf.startswith("FAILED"),
+                "%s didn't cause an error" % line)
+            self.cp.transport.buf = ""
