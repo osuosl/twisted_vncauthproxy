455152377884a78f3c4c883f6f614ff551c4555f
Corbin Simpson <simpsoco@osuosl.org>
2011-03-23 13:53:13 -0700

    control: Use random port allocation, and reclaimation.

--- vncap/control.py
+++ vncap/control.py
@@ -1,4 +1,5 @@
 from json import loads
+from random import choice
 
 from twisted.internet import reactor
 from twisted.internet.error import CannotListenError
@@ -19,8 +20,9 @@ class ControlProtocol(LineReceiver):
             host = d["daddr"]
             dport = d["dport"]
             password = d["password"]
-            if not sport:
-                sport = self.factory.assign_port()
+
+            # Allocate the source port.
+            sport = self.factory.allocate_port(sport)
 
             #factory = VNCProxy(host, dport, password)
             factory = VNCSite(host, dport, password)
@@ -30,6 +32,7 @@ class ControlProtocol(LineReceiver):
             def timeout():
                 log.msg("Timed out connection on port %d" % sport)
                 listening.stopListening()
+                self.factory.free_port(sport)
             reactor.callLater(30, timeout)
 
             log.msg("New forwarder (%d->%s:%d)" % (sport, host, dport))
@@ -38,6 +41,8 @@ class ControlProtocol(LineReceiver):
             log.err("Couldn't handle line %s" % line)
             self.sendLine("FAILED")
         except CannotListenError:
+            # Couldn't bind the port. Don't free it, as it's probably not
+            # available to us.
             log.err("Couldn't bind port %d" % sport)
             self.sendLine("FAILED")
 
@@ -47,10 +52,26 @@ class ControlFactory(ServerFactory):
     protocol = ControlProtocol
 
     def __init__(self):
-        self.pool = set()
-        self.pool_counter = 12000
+        self.pool = set(range(1025, 65536))
 
-    def assign_port(self):
-        # XXX hax
-        self.pool_counter += 1
-        return self.pool_counter
+    def allocate_port(self, port=None):
+        """
+        Allocate a port.
+
+        If a specific port is requested, try to allocate that port. A random
+        port will be selected if it is not available. A random port will also
+        be selected if no specific port is requested.
+        """
+
+        if port not in self.pool:
+            port = choice(self.pool)
+
+        self.pool.discard(port)
+        return port
+
+    def free_port(self, port):
+        """
+        Free a port for further allocations.
+        """
+
+        self.pool.add(port)
