f0bd83c9f633b9541c3651f7232d9753d2e25253
Corbin Simpson <simpsoco@osuosl.org>
2011-05-06 15:47:49 -0700

    Add setup.py and manifest.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="VNCAuthProxy",
+    version="0.1",
+    packages=["vncap"],
+    install_requires=[
+        "Twisted>=10.2",
+    ],
+    author="Corbin Simpson",
+    author_email="simpsoco@osuosl.org",
+    description="A Twisted-based VNC proxy",
+    license="GPL2",
+    url="http://code.osuosl.org/projects/twisted-vncauthproxy",
+)
+
+# Regenerate Twisted plugin cache.
+try:
+    from twisted.plugin import getPlugins, IPlugin
+    list(getPlugins(IPlugin))
+except:
+    pass
