d179196e2d79992214759ad3d0ce5cfa5eee4417
Jack Twilley <jmt@osuosl.org>
2013-12-02 12:56:06 -0800

    Moved generic functions from VNC factory to new file.
    start_proxying and prepare_proxy belong somewhere SSH can see them too.

--- vncap/vnc/factory.py
+++ vncap/vnc/factory.py
@@ -1,59 +1,10 @@
-from twisted.internet.defer import gatherResults
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet.protocol import Factory
 from twisted.python import log
 
+from vncap.proxy import prepare_proxy
 from vncap.vnc.protocol import VNCServerAuthenticator, VNCClientAuthenticator
-
-
-def start_proxying(results):
-    """
-    Callback to start proxies.
-    """
-
-    log.msg("Starting proxy")
-    client, server = results
-
-    # Rewrite the dataReceived and connectionLost hooks to correctly handle
-    # their peers.
-    def cb(peer):
-        if peer.transport:
-            peer.transport.loseConnection()
-
-    server.dataReceived = client.transport.write
-    server.connectionLost = lambda reason=None: cb(client)
-    client.dataReceived = server.transport.write
-    client.connectionLost = lambda reason=None: cb(server)
-
-    # Replay last bits of stuff in the pipe, if there's anything left.
-    data = server._sful_data[1].read()
-    if data:
-        client.transport.write(data)
-    data = client._sful_data[1].read()
-    if data:
-        server.transport.write(data)
-
-    server.transport.resumeProducing()
-    client.transport.resumeProducing()
-    log.msg("Proxying started!")
-
-
-def prepare_proxy(client, server):
-    """
-    Set up the deferred proxy callback.
-    """
-
-    log.msg("Preparing proxies for client %s and server %s"
-        % (client, server))
-    d = gatherResults([client.authentication_d, server.authentication_d])
-    d.addCallback(start_proxying)
-
-    @d.addErrback
-    def cancel_proxy(failure):
-        log.msg("Things went wrong, cancelling proxy")
-        client.transport.loseConnection()
-        server.transport.loseConnection()
 
 
 class VNCClientAuthenticatorFactory(Factory):
